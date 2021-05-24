b457ccb11b742a3516040ea5dbd1ab32cb2f874a
AabhasDanger <arinabansal@gmail.com>
2021-05-24 10:06:02 +0530

    updating fsutils

---
+++ src/custommodules/fsutils.js
@@ -0,0 +1,17 @@
+/*
+*writeFile
+*readFile
+*createFolder
+*DeleteFolder
+*addLine
+*DeleteFile
+*RemoveLine
+*ListFiles
+*/
+
+let fs = require('fs')
+
+//util Function to create/write files
+exports.writeFile = (path,content,edit) => {
+    
+}
